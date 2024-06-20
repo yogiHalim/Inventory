4862c7f742ebe360df7428ef0ef096db9639252a
yogiHalim <yogi@yours.com>
2024-06-20 08:27:39 +0700

    coba frontend to backend berhasil

--- JualBeliNode.js
+++ JualBeliNode.js
@@ -1,88 +1,113 @@
-// var MongoClient = require('mongodb').MongoClient;
-// var url = "mongodb://localhost:27017/";
-
-// MongoClient.connect(url, function(err, db) {
-//   if (err) throw err;
-//   var dbo = db.db("mydb");
-//   var myobj = { name: "Company Inc", address: "Highway 37" };
-//   dbo.collection("customers").insertOne(myobj, function(err, res) {
-//     if (err) throw err;
-//     console.log("1 document inserted");
-//     db.close();
-//   });
-// });
-console.log(1+1);
-console.log(__dirname);
-console.log(__filename);
-
 const fs = require('fs');
-const { json } = require('stream/consumers');
-const aa =  JSON.stringify([
-  {
-      "nama":"yogi",
-      "umur":41,
-      "pets":[
-          { "animal":"dog", "name":"Fido" },
-          { "animal":"cat", "name":"Felix" },
-          { "animal":"hamster", "name":"Lightning" }
-      ]
-  },//'\n',
-  {
-    "nama":"uk",
-    "umur":41
-}])
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-// const ab = JSON.stringify([
+fUtama().catch(console.error);
+//fBaca();
+
+
+async function fUtama(){
+  const vURLmongo = "mongodb+srv://inticollection:1ntiCollection@jualbeli.c8qunga.mongodb.net/?retryWrites=true&w=majority&appName=JualBeli"
+  const vClient = new MongoClient(vURLmongo);
+  await vClient.connect();
+  await fDaftarDBS(vClient);
+  await fBaca(vClient,"yogi");
+//   // untuk run hapus const vTulis =
+//   const vTulis = await fInputJualBeli(vClient,[{
+//     nama:"yogi",
+//     umur:41,
+//   },
 //   {
-//       "nama":"uk",
-//       "umur":41
-//   }
-//   ])
-  const a = aa//+'\n'+ab;
-  console.log(a);
-fs.writeFile('bulanan.txt',a,()=>{
-  console.log('tulis');
-});
+//     nama:"uk",
+//     umur:41,
+//   }]
+//   );
+//   await vClient.close();
+// }
+console.log('connected');
+vClient.close(); //-->?
+};
+
+async function fBaca(vClient,vCari){
+  const vTampil= await vClient.db("IntiCollection").collection("JualBeli").findOne({nama:vCari});
+  if (vTampil){console.log("ada",vTampil.nama,vTampil.umur,vTampil.matchedCount,vTampil);}else{console.log("tidak ada")};
+  const vCursor= await vClient.db("IntiCollection").collection("JualBeli").find({nama:"uk"}).limit(2).toArray();
+  if (vTampil){console.log("ada",vCursor.nama,vCursor.umur,vCursor.matchedCount,vCursor);}else{console.log("tidak ada")};
+  
+  };
+// async function fInputJualBeli(vClient,vData){
+//   const vDataDisimpan = await vClient.db("IntiCollection").collection("JualBeli").insertMany(vData);
+//   console.log(vDataDisimpan.insertedId);
+//   console.log("count yang disimpan",vDataDisimpan.insertedCount, vDataDisimpan.insertedIds);
+// }
+async function fDaftarDBS(vClient){
+  const vDaftarDBS = await vClient.db().admin().listDatabases();
+  vDaftarDBS.databases.forEach(db => {
+    console.log(`- ${db.name}`);
+    //console.log(db.name);
+  }); 
+}
+
+
 
 
 const http = require('http');
-const vServer=http.createServer((req,res)=>{
+const vServer=http.createServer((req,res)=>{  
   console.log(req.url, req.method);
-  res.setHeader('content-type','text/html');
-  //res.write('ahoy');
-  // const baca=fs.readFile('JualBeli.html');
-  // console.log(baca);
-  let vPath ="";
-  switch (req.url) {
-    case '/':
-      vPath+='jualbeli.html';
-      console.log(vPath+'0');
+  
+  if (req.method==='GET'){
+    let vPath ="./";
+    switch (req.url) {
+      case '/':
+        vPath+='jualbeli.html';
+        console.log(vPath+'0');
+        break;
+      case '/about':
+        vPath+='bulanan.txt';
+        console.log(vPath+'2');
+        break;
+      default :vPath+='coba.html';
+      console.log(vPath+'3');
+      //fs.write req.url untuk catat orang nyasar kemana aja
       break;
-    case '/about':
-      vPath+='about.html';
-      console.log(vPath+'2');
-      break;
-    default :vPath+='coba.html';
-    console.log(vPath+'3');
-    //fs.write req.url untuk catat orang nyasar kemana aja
-    break;
-  }
-  fs.readFile(vPath,(err,data)=>{
-    console.log('1'+vPath);
-    if (err) {
-      console.log('err');
-      res.end();
-    }else {
-      console.groupCollapsed('tulis html');
-      //res.write(data);
-      res.end(data);
     }
-  });
-
+  
+    fs.readFile(vPath, function(err, data) {
+      //console.log('1'+this.responseText+data);
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+      } 
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  
+  };
+  if (req.method==='POST' && req.url==='/apiCoba'){
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write('haiiiii');
+    console.log('post ini yaay');
+    res.end();
+  };
 });
+
 vServer.listen(3000,'localhost',()=>{
-  console.log('listening port 3000 ya');
+  console.log('listening port 3000 ya');  
 });
 
 
 //cek cek cek
+//cek vResult.find().sort({createdAt:-1}) --> descending
+
+//tampilkan 5 terakhir diupload, dan siapa yang sekarang sedang lihat/edit dalam 2 menit terakhir dan list 5 yang terakhir lihat
+//beda warna untuk tiap lihat, dan dilog ditampilkan kecil di kanan atas
+//bikin link ajax siapa ini: ditulis iin, yoyok, dst
+
+//1. tambah di html form ---- action="/tambah" method="POST"
+//2. untuk kirim POST json dari textbox harus ada "name" supaya jadi name-value pair
+//3. di node js tambah app.post('/tambah',(req,res)=>{ }) ---->kyny perlu di loop di variabel
+//^di console log untuk cek yang ud sampai node js
+//4. untuk get dengan parameter, pakai const vCari = req.params.vDicari trus coba console.log(vCari) part#10 menit 16//5. untuk delete, store id ke vCursor misalnya trus id vCursor didelete
+//5. res.json({name:value pair}) enak res.send(variabel)??
+//6. lain2 code
+//^window.location.href= part#10 menit 32:40
---
+++ coba.js
@@ -0,0 +1,48 @@
+const vPath=__dirname+'\\bulanan.txt';
+const wPath=__dirname;
+//const zPath=wPath+%5C+vPath;
+
+console.log(vPath);
+console.log(wPath);
+//console.log(zPath);
+
+//const vResponse = fetch(vPath);
+//console.log(vResponse);
+let v1 = 0;
+const vHttp = require('http');
+const fs=require('fs');
+const vServer=vHttp.createServer((req,res)=>{  
+  console.log(req.url, req.method);
+if (req.method==='GET' && req.url==='/'){
+  res.writeHead(200,{'Contetnt-Type':'text/html'});
+  fs.readFile('coba.html',function(err,vHalHtml){
+    res.write(vHalHtml);
+    res.end();
+  });
+};
+console.log('ke if post',v1++);
+if (req.method==='POST' && req.url==='/apiCoba'){
+  res.writeHead(200,{'Content-Type':'text/plain'}); //ganti dari text/html
+  res.write('haiiiii');
+  console.log('post ini yaay');
+  res.end();
+};
+});
+
+vServer.listen(3000,'localhost',()=>{
+    console.log('listening port 3000 ya');
+});
+
+
+// const vServer2=http.createServer((req,res)=>{  
+//   console.log(req.url, req.method);
+//   res.write('listening to port 3300 ya');
+// });
+
+// vServer2.listen(3300,'localhost',()=>{
+//     console.log('listening port 3300 ya');
+// });
+
+const a = 'aa';
+const b = 'bb';
+module.exports={aaz : a,bbz : b};
